1691a6c49453d98108183c728c3e2385ef77d275
Abhishek Singh <abhishekkumarsingh.cse@gmail.com>
2016-02-01 20:38:04 +0530

    code refined and refactored

--- machine_translation.py
+++ machine_translation.py
@@ -2,9 +2,9 @@ import numpy as np
 from scipy.stats import entropy
 from util import SourceWLexicon, TargetWLexicon, SOURCE_PATH, TARGET_PATH
 
+
 # V is vocabulary size
 V = len(SourceWLexicon)
-# print V
 # Revised probablity matrix
 RPMat = np.zeros((2, V, V))
 # Initial uniform probability matrix
@@ -16,14 +16,19 @@ EcountMat = {}
 
 
 def convert_word_to_wordId(source_line, target_line):
+    """returns a list source word ids and target word
+       ids.
+    """
     source_words = source_line.split()
     target_words = target_line.split()
     source_id = [SourceWLexicon[word.lower()] for word in source_words]
     target_id = [TargetWLexicon[word.lower()] for word in target_words]
     return source_id, target_id
 
+
 def build_freqTable(source_line, target_line):
-    """ TODO
+    """takes source line and target line as parameter and
+       returns frequency table for source and target words
     """
     source_w = {}
     target_w = {}
@@ -34,11 +39,13 @@ def build_freqTable(source_line, target_line):
         target_w[id] = target_w.get(id, 0) + 1
     return source_w, target_w
 
+
 def cal_expected_count(source_line, target_line):
+    """expected count calculation using intitalised uniform
+       probability. Storing expected count in EcountMat
+    """
     global InMat, EcountMat
     source_w, target_w = build_freqTable(source_line, target_line)
-    #print "source_w", source_w
-    #print "target_w", target_w
     # check for unique words
     for s_id in source_w:
         for t_id in target_w:
@@ -47,10 +54,12 @@ def cal_expected_count(source_line, target_line):
             exp_count = source_w[s_id] * target_w[t_id] * p
             key = (s_id, t_id)
             EcountMat[key] = exp_count
-    #print "============Expected count==========="
-    #print EcountMat
+
 
 def fill_numDeno_RPMat():
+    """Writes count(w, a) as numerator and
+       column sum as denominator to RPMat matrix
+    """
     global EcountMat, RPMat
     jth_col_sum = {}
     for key in EcountMat:
@@ -65,31 +74,34 @@ def fill_numDeno_RPMat():
                 if k[1] == j:
                     denom = denom + EcountMat[k]
                     jth_col_sum[j] = denom
-
-        # RPMat[1][:, :j+1] = RPMat[1][i][j] + denom
-        # print "==========Revised Prob=========="
-
-
     for j in jth_col_sum:
         denom = jth_col_sum[j]
         RPMat[1][:, j:j+1] = RPMat[1][i][j] + denom
-    #print RPMat
+
 
 def clear_EcountMat():
+    """clears EcountMat matrix
+    """
     EcountMat.clear()
 
+
 def revisedProbability(sf, tf):
+    """calculates revised probability using
+       the expected count values of pairs.
+       Returns updated probability matrix
+    """
     global RPMat
     for sl, tl in zip(sf, tf):
-        #print "in here"
         cal_expected_count(sl, tl)
         fill_numDeno_RPMat()
         clear_EcountMat()
-    # print "*****"
-    #print RPMat[1]
     return RPMat[0]/RPMat[1]
 
+
 def calculate_entropy(mat):
+    """calculates entropy for each column
+       of the updated probability matrix.
+    """
     entropy_colwise = []
     len_col = mat.shape[-1]
     for i in xrange(len_col):
@@ -97,7 +109,10 @@ def calculate_entropy(mat):
         entropy_colwise.append(en[0])
     return entropy_colwise
 
+
 def main():
+    """Driver to run the IBM model 1
+    """
     sf = open(SOURCE_PATH, 'r')
     tf = open(TARGET_PATH, 'r')
     global InMat, RPMat
@@ -111,21 +126,14 @@ def main():
         RPMat.fill(0.)
         sf.seek(0)
         tf.seek(0)
-        #InMat = revisedProbability(sf, tf)
-        #print InMat
         if np.allclose(tmpMat, InMat):
             break
-
-    #for sl, tl in zip(sf, tf):
-    #    cal_expected_count(sl, tl)
-    #    fill_numDeno_RPMat()
-    #    clear_EcountMat()
-    # complete revised prob matrix
-    #ResMat = RPMat[0]/RPMat[1]
     sf.close()
     tf.close()
     print "========Final Prob Matrix======"
     print InMat
     print "Total Iteration: ", c
 
-main()
+
+if __name__ == '__main__':
+    main()
